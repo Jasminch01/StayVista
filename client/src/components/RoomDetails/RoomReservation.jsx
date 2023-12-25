fcea6d76f62be357ff920a461a2310cc8ac48554
jasmin <jasminchakma895@gmail.com>
2023-12-25 14:07:24 +0600

    added calender on dynamic room route

---
+++ client/src/components/RoomDetails/RoomReservation.jsx
@@ -0,0 +1,43 @@
+import Calender from "./Calender";
+import Button from "../Button/Button";
+// import { useState } from "react";
+// import { formatDistance } from "date-fns";
+
+const RoomReservation = ({ room }) => {
+//     const [value, setValue] = useState({
+//       startDate: new Date(room?.from),
+//       endDate: new Date(room?.to),
+//       key: 'selection',
+//     })
+
+//   const totalDays = parseInt(
+//     formatDistance(new Date(room?.to), new Date(room?.from)).split(" ")[0]
+//   );
+//   //   Total Price Calculation
+//   const totalPrice = totalDays * room?.price;
+
+  return (
+    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {room.price}</div>
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+      <div className="flex justify-center">
+        <Calender />
+      </div>
+
+      <hr />
+      <div className="p-4">
+        <Button label="Reserve" />
+      </div>
+      <hr />
+      <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+        <div>Total</div>
+        <div>$</div>
+      </div>
+    </div>
+  );
+};
+
+export default RoomReservation;
--- client/src/pages/RoomsDetails/RoomsDetails.jsx
+++ client/src/pages/RoomsDetails/RoomsDetails.jsx
@@ -2,6 +2,9 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Container from "../../components/Shared/Container";
 import Loader from "../../components/Loading/Loading";
+import Header from "../../components/RoomDetails/Header";
+import RoomInfo from "../../components/RoomDetails/RoomInfo";
+import RoomReservation from "../../components/RoomDetails/RoomReservation";
 
 const RoomsDetails = () => {
   const { id } = useParams();
@@ -22,17 +25,16 @@ const RoomsDetails = () => {
   if (loading) return <Loader />;
   return (
     <Container>
-      <div>
-        <div className="flex justify-center">
-          <img src={room.image} alt="" className="w-full h-[50vh] rounded-lg" />
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          {" "}
+          <Header room={room} />
         </div>
-        <div className="flex justify-between">
-          <div className="w-1/2">
-            <p className="text-2xl font-bold">{room.title}</p>
-            <p>{room.description}</p>
-          </div>
-          <div className="w-1/2">
-            <p className="text-center">calender</p>
+        <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+          {" "}
+          <RoomInfo room={room} />
+          <div className="md:col-span-3 order-first md:order-last">
+            <RoomReservation room={room}></RoomReservation>
           </div>
         </div>
       </div>
